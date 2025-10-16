270e9794156d77b88754b9825d4a89c54fb64e2a
lraj22 <63673413+lraj22@users.noreply.github.com>
2025-10-16 01:11:05 -0700

    Initial commit

---
+++ src/main.js
@@ -0,0 +1,12 @@
+// main.js - handles the main tasks
+import "./main.css";
+
+var dom = {};
+document.querySelectorAll("[id]").forEach(el => dom[el.id] = el);
+
+const canvas = document.querySelector("canvas"); // dom.canvas
+const ctx = canvas.getContext("2d");
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+
+// canvas & context ready!
